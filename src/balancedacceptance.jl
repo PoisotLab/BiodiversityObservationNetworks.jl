c42dd4ed8fb622602f136edc44aa6fc4e4a80028
Timothée Poisot (he/him) <timothee.poisot@umontreal.ca>
2022-08-22 15:31:07 -0400

    🦆 dispatch

--- src/balancedacceptance.jl
+++ src/balancedacceptance.jl
@@ -9,29 +9,22 @@ function _validate(sampler::BalancedAcceptance)
     return nothing
 end
 
-function seed!(sampler::BalancedAcceptance, entropy::Matrix{T}) where {T <: AbstractFloat}
-    function _generate!(uncertainty::Matrix{T})
-        seed = Int32.([floor(10^7*rand()),floor(10^7*rand())])
-        np, α = sampler.numpoints, sampler.α
-        x,y = size(uncertainty)
+function _generate!(coords::Vector{CartesianIndex}, sampler::BalancedAcceptance, uncertainty::Matrix{T})
+    seed = Int32.([floor(10^7 * rand()), floor(10^7 * rand())])
+    np, α = sampler.numpoints, sampler.α
+    x, y = size(uncertainty)
 
-        stduncert = StatsBase.transform(StatsBase.fit(ZScoreTransform, uncertainty, dims=2), uncertainty)
-        reluncert = broadcast(x->exp(α*x)/(1+exp(α*x)), stduncert)
-        coords = []
+    stduncert = StatsBase.transform(StatsBase.fit(ZScoreTransform, uncertainty, dims=2), uncertainty)
+    reluncert = broadcast(x -> exp(α * x) / (1 + exp(α * x)), stduncert)
 
-        ptct = 0
-        while length(coords) < np
-            i,j = haltonvalue(seed[1]+ptct,2), haltonvalue(seed[2]+ptct,3)
-            candcoord = CartesianIndex(convert.(Int32, [ceil(x*i), ceil(y*j)])...)
-            prob = reluncert[candcoord]
-            rand() < prob && push!(coords, candcoord)
-            ptct += 1
-        end
-        
-        return coords
+    ptct = 0
+    while length(coords) < np
+        i, j = haltonvalue(seed[1] + ptct, 2), haltonvalue(seed[2] + ptct, 3)
+        candcoord = CartesianIndex(convert.(Int32, [ceil(x * i), ceil(y * j)])...)
+        prob = reluncert[candcoord]
+        rand() < prob && push!(coords, candcoord)
+        ptct += 1
     end
-    return 
+
+    return coords
 end
-
-
-
