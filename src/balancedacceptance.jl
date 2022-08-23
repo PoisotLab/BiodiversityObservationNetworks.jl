9b96258e058d8a7b806036eadad1322216c0aad8
Timothée Poisot (he/him) <timothee.poisot@umontreal.ca>
2022-08-22 21:30:20 -0400

    🦆 make BalancedAcceptance fully parametric

--- src/balancedacceptance.jl
+++ src/balancedacceptance.jl
@@ -1,6 +1,15 @@
-Base.@kwdef mutable struct BalancedAcceptance{I <: Integer} <: BONSeeder
+"""
+    BalancedAcceptance
+
+...
+
+**numpoints**, an Integer (def. 50), specifying the number of points to use.
+
+**α**, an AbstractFloat (def. 1.0), specifying ...
+"""
+Base.@kwdef mutable struct BalancedAcceptance{I <: Integer, F <: AbstractFloat}# <: BONSeeder
     numpoints::I = 50
-    α = 1.0
+    α::F = 1.0
     function BalancedAcceptance(numpoints, α)
         if numpoints < one(numpoints)
             throw(ArgumentError("You cannot have a BalancedAcceptance with fewer than one point"))
@@ -8,7 +17,7 @@ Base.@kwdef mutable struct BalancedAcceptance{I <: Integer} <: BONSeeder
         if α <= zero(α)
             throw(ArgumentError("The value of α for BalancedAcceptance must be larger than 0"))
         end
-        return new{typeof(numpoints)}(numpoints, α)
+        return new{typeof(numpoints), typeof(α)}(numpoints, α)
     end
 end
 
