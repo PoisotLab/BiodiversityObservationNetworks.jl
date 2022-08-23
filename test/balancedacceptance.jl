437fda7947d42c98323b0c53382b6bc4469563fb
Timothée Poisot (he/him) <timothee.poisot@umontreal.ca>
2022-08-22 21:39:03 -0400

    🟩 Balanced Acceptance sampling (error messages)

--- test/balancedacceptance.jl
+++ test/balancedacceptance.jl
@@ -1,4 +1,15 @@
+module BONTestBalancedAcceptance
+
 using BiodiversityObservationNetworks
 using Test
 
-# Empty test file
+# Must have one point or more
+@test_throws ArgumentError BalancedAcceptance(0, 1.0)
+
+# Must have a positive alpha
+@test_throws ArgumentError BalancedAcceptance(1, 0.0)
+
+# Parametric constructor
+@test typeof(BalancedAcceptance(2, 0.2f0)) == BalancedAcceptance{Int64,Float32}
+
+end
