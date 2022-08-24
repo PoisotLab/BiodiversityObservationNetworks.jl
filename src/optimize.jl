4db9c4588069355838cf9afc005c7f231292ef2c
michael <michael@pop-os.localdomain>
2022-08-24 11:44:29 -0400

    :pencil: Optimization API goals

--- src/optimize.jl
+++ src/optimize.jl
@@ -1,1 +1,13 @@
-# 
+"""
+    What should the optimization API look like?
+
+
+    We have a bunch of rasters that are layers that we 
+    can to combine in a layer 'stack'.
+
+    We can to have a weights matrix W which has `r` rows and
+    `t` columns, where `r` is the number of layers in the stack,
+    and `t` is the number of optimization targets.
+
+    We want a function `optimize` which takes
+"""
