f0bb7c5e6ffce1ee96b52d9f5b8757957bb9d321
Timothée Poisot (he/him) <timothee.poisot@umontreal.ca>
2022-08-24 11:01:53 -0400

    🧑‍🏫 vignette setup using Literate

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,38 @@
-using Documenter, BiodiversityObservationNetworks 
+using Documenter, BiodiversityObservationNetworks
+import Literate
 
 # For GR docs bug
 ENV["GKSwstype"] = "100"
 
-makedocs(
-    sitename="BiodiversityObservationNetworks",
-    authors="Timothée Poisot + others",
-    modules=[BiodiversityObservationNetworks],
-    pages=[
+vignettes = filter(
+    endswith(".jl"),
+    readdir(joinpath(@__DIR__, "src", "vignettes"); join = true, sort = true),
+)
+for vignette in vignettes
+    Literate.markdown(
+        vignette,
+        joinpath(@__DIR__, "src", "vignettes");
+        config = Dict("credit" => false, "execute" => true),
+    )
+end
+
+makedocs(;
+    sitename = "BiodiversityObservationNetworks",
+    authors = "Timothée Poisot + others",
+    modules = [BiodiversityObservationNetworks],
+    pages = [
         "Index" => "index.md",
+        "Vignettes" => [
+            "Overview" => "vignettes/overview.md",
+        ],
     ],
-    checkdocs=:all,
+    checkdocs = :all,
     #strict=true,
 )
 
-deploydocs(
-    deps=Deps.pip("pygments", "python-markdown-math"),
-    repo="github.com/EcoJulia/BiodiversityObservationNetworks.jl.git",
-    devbranch="main",
-    push_preview=true
+deploydocs(;
+    deps = Deps.pip("pygments", "python-markdown-math"),
+    repo = "github.com/EcoJulia/BiodiversityObservationNetworks.jl.git",
+    devbranch = "main",
+    push_preview = true,
 )
