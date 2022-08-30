d0b7185f365e039816aacf9a4137b07ad1a6accb
michael <michael@pop-os.localdomain>
2022-08-30 10:54:48 -0400

    :pencil: lowering expectations

--- docs/src/vignettes/optimization.jl
+++ docs/src/vignettes/optimization.jl
@@ -1,18 +1,23 @@
 # # Optimization of layer weighting
 
-# At the moment, we can consider determinsitic
-# loss functions.
+# At the moment, we can only consider determinsitic
+# loss functions because the other type of optimization 
+# is hard and only sometimes converges ¯\_(ツ)_/¯
+#
+# For example, what if you want to optimize your
+# spatial locations such that they have the least
+# amount of covariance in environmental conditons-
+# that is, they are as environmentally _unique_ as 
+# possible. That is what we'll be building in this
+# vignette.
+
 
 using BiodiversityObservationNetworks
 using NeutralLandscapes
 using Plots
-
-
-# ...?
-
-using Statistics, StatsBase
-using NeutralLandscapes
+using SimpleSDMLayers
 using Zygote, SliceMap
+using Statistics, StatsBase
 
 
 dims, nl, nt = (50, 50), 5, 3
@@ -27,5 +32,5 @@ model =
     (layers, W, α) ->
         StatsBase.entropy(squish(layers,W,α)) / prod(size(layers[:, :, 1]))
 
-out = optimize(layers, model; numsteps = 10^4)
-heatmap(out)
+#out = optimize(layers, model; numsteps = 10^4)
+#heatmap(out)
