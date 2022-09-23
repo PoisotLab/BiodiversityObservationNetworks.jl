3670a120b273c384df836f8412c4c0581dba6a5c
karinorman <kari@normanfamily.org>
2022-09-23 13:11:41 -0400

    Got the flight phase working!

--- src/cube.jl
+++ src/cube.jl
@@ -1,6 +1,5 @@
 using LinearAlgebra
-using DataFrames
-using Pipe
+using StatsBase
 
 # Need a vector of inclusion probabilities for a set of points
 
@@ -22,60 +21,101 @@ p = 3
 x = rand(0:4, p, N)
 
 ### Flight Phase ###
+ i = 0
+ set_nullspace = zeros(1,2)
+# check if there is a possible u to satisfy the conditions
+while size(set_nullspace)[2] != 0
+    i = i+1
+    #yield(i)
+    ## STEP 1 ##
 
-## STEP 1 ##
+    # find a vector u that is in the kernel of the matrix A
+    # A is the matrix of auxillary variables didvided by the inclusion probability
+    # for the population unit
+    A = similar(x, Float64)
+    for i = 1:N
+        if pik[i] .∈ Ref(Set([0,1]))
+            A[:,i] = zeros(p) # p is the row dimension
+        else 
+            A[:,i] = x[:,i] ./ pik[i]
+        end
+    end
 
-# find a vector u that is in the kernel of the matrix A
-# A is the matrix of auxillary variables didvided by the inclusion probability
-# for the population unit
-A = similar(x, Float64)
-for i = 1:N
-    A[:,i] = x[:,i] ./ pik[i]
-end
+    # get the nullspace of A
+    kernal = nullspace(A)
 
-## FIXME: this does not deal with the case of a zero or one inclusion probability
-##        which should result in u_k(t) = 0, and currenlty will divide by zero to get A
+    # u is in the kernal of A, but also u_k = 0 when π_k is {0,1}
+    # let's make sure the rows that need it satisfy that condition
 
-# get the nullspace of A
-kernal = nullspace(A)
+    # get index where pik is 0 or 1
+    set_piks = findall(x -> x .∈ Ref(Set([0,1])), pik)
 
-# randomly pick a vector u in that kernel that is non-zero
-# get vector of column sums
-sums = sum(eachrow(kernal))
-# find zeros
-zero_ind = findall(x -> x==0, sums)
+    # if none of the pik's are fixed yet (as 0 or 1) u can be a vector from the nullspace
+    if length(set_piks) == 0
+        u = kernal[:, rand(1:size(kernal)[2])]
+    
+    # if only one is fixed, can also pick a u vector but it shouldn't be the trivial indicator vector
+    elseif length(set_piks) == 1
+        sums = sum(eachrow(kernal))
+        # find indicator vector
+        ind = findall(x -> x==1, sums)
 
-# get vector of potential column indexes, remove zero columns, and get random u
-ind = deleteat!(collect(1:N), zero_ind)
-u = kernal[:, rand(ind)]
+        # get vector of potential column indices, remove unit column, and get random u
+        ind = deleteat!(collect(1:size(kernal)[2]), ind)
+        u = kernal[:, rand(ind)]
 
-## STEP 2 ##
+    # otherwise, need to make sure u_k = 0 condition is satisfied for fixed pik's
+    else
+        # get rows of A's nullspace corresponding to those pik's
+        set_A = kernal[set_piks, :]
+        # get the nullspace of that matrix
+        print(i)
+        set_nullspace = nullspace(set_A)
+        print(i)
 
-# want max  λ_1, λ_2 such that -pik <= λ_1 * u <= 1 - pik and -pik <= -λ_2 * u <= 1 - pik
-# solve the inequalities for λ and you get max values for u > 0 and u < 0
-# for λ_1 : for u > 0, λ_1 = (1-pik)/u; for u < 0, λ_1 = -pik/u
-# for λ_2 : for u > 0, λ_2 = pik/u; for u < 0, λ_2 = (pik - 1)/u
+        if size(set_nullspace)[2] == 0
+            break
+        end
 
-λ1_max(; u, pik) = @. ifelse(u > 0, (1 - pik) / u, - pik / u)
-λ2_max(; u, pik) = @. ifelse(u > 0, pik / u, (pik - 1) / u)
+        # randomly pick a vector from the second nullspace
+        v = set_nullspace[:, rand(1:size(set_nullspace)[2])]
 
-#vars_df = DataFrame(pik = pik, u = u, λ1 = λ1_max(u = u, pik = pik), λ2 = λ2_max(u = u, pik = pik)) 
-λ1 = minimum(λ1_max(u = u, pik = pik))
-λ2 = minimum(λ2_max(u = u, pik = pik))
+        # multiply it by original kernal to get a vector u that satisfies the zeroes requirement
+        ### FIX ME: need to deal with rounding issues, the zeros are not real zeroes!
+        u = kernal * v
+        # this is a hacky way to make sure zeros are real zeroes 
+        u[set_piks] .= 0
+    end
 
-## STEP 3 ##
+    ## STEP 2 ##
 
-# calculate the inequality expression for both lambdas
-λ1_ineq = @. pik + ( λ1 * u)
-λ2_ineq = @. pik - ( λ2 * u)
+    # want max  λ_1, λ_2 such that -pik <= λ_1 * u <= 1 - pik and -pik <= -λ_2 * u <= 1 - pik
+    # solve the inequalities for λ and you get max values for u > 0 and u < 0
+    # for λ_1 : for u > 0, λ_1 = (1-pik)/u; for u < 0, λ_1 = -pik/u
+    # for λ_2 : for u > 0, λ_2 = pik/u; for u < 0, λ_2 = (pik - 1)/u
 
-ineq_mat = reduce(hcat, [λ1_ineq, λ2_ineq])
-# the new inclusion probability π is one of the lambda expressions with a given probability q1, q2
-q1 = λ2/(λ1 + λ2)
-q2 = 1 - q1
+    λ1_max(; u, pik) = @. ifelse(u > 0, (1 - pik) / u, - pik / u)
+    λ2_max(; u, pik) = @. ifelse(u > 0, pik / u, (pik - 1) / u)
 
-new_pik = sample.(eachrow(ineq_mat), [Weights([q1,q2])])
+    #vars_df = DataFrame(pik = pik, u = u, λ1 = λ1_max(u = u, pik = pik), λ2 = λ2_max(u = u, pik = pik)) 
+    λ1 = minimum(filter(x -> isfinite(x), λ1_max(u = u, pik = pik)))
+    λ2 = minimum(filter(x -> isfinite(x), λ2_max(u = u, pik = pik)))
 
+    ## STEP 3 ##
+
+    # calculate the inequality expression for both lambdas
+    λ1_ineq = @. pik + ( λ1 * u)
+    λ2_ineq = @. pik - ( λ2 * u)
+
+    ineq_mat = reduce(hcat, [λ1_ineq, λ2_ineq])
+    # the new inclusion probability π is one of the lambda expressions with a given probability q1, q2
+    q1 = λ2/(λ1 + λ2)
+    q2 = 1 - q1
+
+    pik = map(r -> sample(r, Weights([q1,q2])), eachrow(ineq_mat))
+    #yield(i)
+
+end 
 # double check that it satisfies the inequality 
 #= if minimum(λ1_ineq) < 0
     print("λ1 lower bound problem") 
