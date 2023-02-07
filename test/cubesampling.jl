1e020e975505f47adc8d54e93c6fb3a3f3373ac6
karinorman <kari@normanfamily.org>
2023-02-07 12:05:25 -0500

    Figuring out argument order issue.

--- test/cubesampling.jl
+++ test/cubesampling.jl
@@ -7,7 +7,10 @@ using Test
 @test_throws ArgumentError CubeSampling(numpoints = 0)
 
 # Must select fewer points than number of candidate points
-@test_throws ArgumentError CubeSampling(numpoints = 20, pik = zeros(10))
+@test_throws ArgumentError CubeSampling(numpoints = 20, pik = zeros(10), x = rand(0:4, 2, 10))
+
+# Must have the same number of inclusion probabilites as auxillary variables
+@test_throws ArgumentError CubeSampling(numpoints = 5, pik = zeros(15), x = rand(0:4, 2, 10))
 
 # Correct subtype
 @test typeof(CubeSampling(numpoints = 0)) <: BONRefiner
@@ -18,10 +21,18 @@ N = 100
 U = rand(20, 20)
 pack = seed(BalancedAcceptance(; numpoints = N), U)
 c = Vector{CartesianIndex}(undef, 15)
-x = rand(0:4, 2, N)
-smpl = CubeSampling(numpoints = length(c), x = x)
+x_mat = rand(0:4, 2, N)
+smpl = CubeSampling(numpoints = length(c), x = x_mat)
 
 
 # Length and element type
 @test length(first(refine(pack, smpl))) == smpl.numpoints
 @test eltype(first(refine(first(pack), smpl, U))) == CartesianIndex
+
+# Test with an existing coordinates vector
+@test_throws DimensionMismatch refine!(
+    c,
+    first(pack),
+    CubeSampling(; numpoints = length(c) - 1, x = x_mat),
+    last(pack),
+)
