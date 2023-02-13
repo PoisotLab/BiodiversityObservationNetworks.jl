f945eb5d884bb4e6d1f2af76f544a2a6753b5030
karinorman <kari@normanfamily.org>
2023-02-13 11:49:18 -0500

    Clean up testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using BiodiversityObservationNetworks
 tests = [
     "\033[1m\033[34mSEEDER\033[0m  Balance Acceptance Sampling" => "balancedacceptance.jl",
     "\033[1m\033[35mREFINER\033[0m Adaptive Spatial Sampling" => "adaptivespatial.jl",
+    "\033[1m\033[36mHELPER\033[0m Cube Sampling " => "cubesampling.jl",
     "\033[1m\033[35mREFINER\033[0m Uniqueness" => "uniqueness.jl",
     "\033[1m\033[36mHELPER\033[0m Entropize" => "entropize.jl",
     "\033[1m\033[36mHELPER\033[0m Squish" => "squish.jl",
