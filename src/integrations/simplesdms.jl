227291c6eb994c37f39b8b075db5a89835e18bab
michael <michael@pop-os.localdomain>
2023-06-05 12:08:50 -0400

    :sparkle: Update to SDT

--- src/integrations/simplesdms.jl
+++ src/integrations/simplesdms.jl
@@ -1,7 +1,6 @@
 @info "Loading BONs.jl support for SimpleSDMLayers.jl ..."
 
-
-function BiodiversityObservationNetworks.stack(layers::Vector{<:SimpleSDMLayers.SimpleSDMLayer})
+function BiodiversityObservationNetworks.stack(layers::Vector{<:SpeciesDistributionToolkit.SimpleSDMLayers.SimpleSDMLayer})
     # assert all layers are the same size if we add this function to BONs.jl
     mat = zeros(size(first(layers))..., length(layers))
     for (l,layer) in enumerate(layers)
--- test/stack.jl
+++ test/stack.jl
@@ -1,21 +1,20 @@
 module BONTestStack
     using BiodiversityObservationNetworks
-    using SimpleSDMLayers
+    using SpeciesDistributionToolkit
     using Test
 
     nl = 10
     layers = [rand(50,50) for i in 1:nl]
 
-    @test typeof(stack(layers)) <: Array{T,3} where T
-    @test size(stack(layers),3) == nl
+    @test typeof(BiodiversityObservationNetworks.stack(layers)) <: Array{T,3} where T
+    @test size(BiodiversityObservationNetworks.stack(layers),3) == nl
 
     bbox = (left=-83.0, bottom=46.4, right=-55.2, top=63.7)
-    temp, precip, seasonality, elevation = 
-        convert(Float32, SimpleSDMPredictor(WorldClim, BioClim, 7; bbox...)),
-        convert(Float32, SimpleSDMPredictor(WorldClim, BioClim, 12; bbox...)),
-        convert(Float32, SimpleSDMPredictor(WorldClim, BioClim, 4; bbox...)),
-        convert(Float32, SimpleSDMPredictor(WorldClim, Elevation; bbox...))
+    temp, precip, elevation = 
+        SimpleSDMPredictor(rand(50,50); bbox...),
+        SimpleSDMPredictor(rand(50,50); bbox...),
+        SimpleSDMPredictor(rand(50,50); bbox...)
     
-    layers = [temp,precip,seasonality,elevation] 
-    @test typeof(stack(layers)) <: Array{T,3} where T
+    layers = [temp,precip,elevation] 
+    @test typeof(BiodiversityObservationNetworks.stack(layers)) <: Array{T,3} where T
 end 
