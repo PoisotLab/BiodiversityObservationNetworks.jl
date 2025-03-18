c132794f7db6b3a47c8737239ef5edc6292593b8
michael d. catchen <mdcatchen@gmail.com>
2025-03-18 08:11:41 -0400

    feat: single gadm overload

--- src/overloads.jl
+++ src/overloads.jl
@@ -5,14 +5,6 @@ A wrapper around the SpeciesDistributionToolkit `gadm` method. Ensures the
 resulting object is using the ArchGDAL representation, which makes intersecting
 Polygon's easier later on. 
 """
-function gadm(arg)
-    json_str = SDT.GeoJSON.write(SDT.gadm(arg)[1].geometry)
-    poly = GI.MultiPolygon(AGDAL.fromJSON(json_str))
-    Polygon(GI.trait(poly), poly)
-end
-
-function gadm(arg...)
-    json_str = SDT.GeoJSON.write.(SDT.gadm(arg...))
-    poly = GI.MultiPolygon.(AGDAL.fromJSON.(json_str))
-    Polygon(GI.trait(first(poly)), poly)
+function gadm(args...)
+    Polygon(SDT.gadm(args...))
 end
