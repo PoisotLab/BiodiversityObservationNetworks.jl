ef44a5b3b525f1872a387a6e7887aebc50abb2b1
michael d. catchen <mdcatchen@gmail.com>
2025-03-18 08:12:20 -0400

    feat: clustering update

--- src/utilities/clustering.jl
+++ src/utilities/clustering.jl
@@ -1,7 +1,7 @@
+#= 
 function _feature_distance(feat)
     nfeat = size(feat, 2)
     feat_dist = zeros(nfeat, nfeat)
-
     for i in 1:nfeat, j in i+1:nfeat
         dist = sqrt(sum((feat[:,i] .- feat[:,j]).^2)) 
         feat_dist[i,j] = dist
@@ -9,35 +9,37 @@ function _feature_distance(feat)
     end 
     return feat_dist
 end
+=#
 
-abstract type ClusteringAlgorithm end 
-@kwdef struct KMeans{I<:Integer} <: ClusteringAlgorithm 
-    k::I = 3
-end
-
-@kwdef struct KMedoids{I<:Integer} <: ClusteringAlgorithm 
-    k::I = 3
-end
-
-function cluster!(km::KMeans, layers::RasterStack, assignments::Raster)
-    k = km.k 
-    idx, mat = features(layers)
-    clust = Clustering.kmeans(mat, k)
-    assignments.raster.grid[idx] .= clust.assignments
-    return assignments
-end 
-
-
-function cluster!(km::KMedoids, layers::RasterStack, assignments::Raster)
-    k = km.k 
-    idx, mat = features(layers)
-    dist_mat = _feature_distance(mat)
-    clust = Clustering.kmedoids(dist_mat, k)
-    assignments.raster.grid[idx] .= clust.assignments
-    return assignments
-end 
-
+abstract type ClusteringAlgorithm end
 function cluster(alg::ClusteringAlgorithm, layers::RasterStack)
     assignments = deepcopy(first(layers))
     cluster!(alg, layers, assignments)
 end
+
+
+@kwdef struct KMeans{I<:Integer} <: ClusteringAlgorithm 
+    k::I = 3
+end
+
+function cluster(km::KMeans, stack::RasterStack; kw...)
+    k = km.k 
+    layers = map(x->x.raster, stack.stack)
+    clustering_result = Clustering.kmeans(layers, k; kw...)
+    return Raster(SDMLayer(clustering_result, layers))
+end 
+
+
+@kwdef struct FuzzyCMeans{I<:Integer,F<:Real} <: ClusteringAlgorithm 
+    c::I = 3
+    fuzzyness::F = 2.0
+end
+
+function cluster(cm::FuzzyCMeans, stack::RasterStack; kw...)
+    c, fuzzyness = cm.c, cm.fuzzyness
+    layers = map(x->x.raster, stack.stack)
+    clustering_result = Clustering.fuzzy_cmeans(layers, c, fuzzyness; kw...)
+    return RasterStack(SDMLayer(clustering_result, layers))
+end 
+
+
