4939740e8b215904b4b9c5be2401b282dedf0d7f
Michael D. Catchen <mdcatchen@gmail.com>
2025-12-07 14:14:24 -0500

    docs

--- docs/processing.jl
+++ docs/processing.jl
@@ -9,7 +9,7 @@ end
 function pre_collapse_figure(content)
     fig_hash = string(hash(rand(100)))
     
-    matcher = r"^# fig-(?<title>[\w-]+)$\n(?<code>(?>^[^#].*$\n){1,})^current_figure\(\) #hide$"m
+    matcher = r"^# fig-(?<title>[\w-]+)$\n(?<code>(?>^[^#].*$\n){1,})^current_figure\(\) #hide$"imx
 
     replacement_template = """
     # ![](HASH-\\g<title>.png)
